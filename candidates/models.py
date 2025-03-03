0240839617016a4e0d6091e831106d1c5dbd780e
Shekhar Nunia <shekhar.nunia@auctane.com>
2025-03-03 17:37:52 +0530

    gender choice update

--- candidates/models.py
+++ candidates/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class GenderChoices(models.TextChoices):
-    MALE = "M", "Male"
-    FEMALE = "F", "Female"
-    OTHER = "O", "Other"
+    MALE = "MALE", "Male"
+    FEMALE = "FEMALE", "Female"
+    OTHER = "OTHER", "Other"
 
 
 class Candidate(models.Model):
     name = models.CharField(max_length=100)
     # age = models.IntegerField()
     date_of_birth = models.DateField()  # age can be calculated from this field
-    gender = models.CharField(max_length=1, choices=GenderChoices.choices)
+    gender = models.CharField(max_length=6, choices=GenderChoices.choices)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15)
 
