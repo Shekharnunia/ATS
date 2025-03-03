7d759c9a7cb1bdcdaeff5b9053cc70130b07608b
Shekhar Nunia <shekhar.nunia@auctane.com>
2025-03-03 17:03:14 +0530

    candidate model added

--- candidates/models.py
+++ candidates/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class GenderChoices(models.TextChoices):
+    MALE = "M", "Male"
+    FEMALE = "F", "Female"
+    OTHER = "O", "Other"
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    # age = models.IntegerField()
+    date_of_birth = models.DateField()  # age can be calculated from this field
+    gender = models.CharField(max_length=1, choices=GenderChoices.choices)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
